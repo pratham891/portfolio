4f0d012fd383c743431bb0e73d0f4cd98b53cd29
Pratham Singh <prathambsingh891@gmail.com>
2024-09-04 12:25:21 +0530

    add about section skeleton

---
+++ src/sections/about/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+    return (
+        <div className='About' style={{ border: "1px solid #fff" }}>
+            <div className="about-heading">
+                <p>Hey, I'm Pratham Singh</p>
+            </div>
+
+            <div className="about-desc">
+                <p>I am a Software Engineer exploring about tech and opportunities.</p>
+            </div>
+
+            <div className="about-options">
+                <div className="hire-me">
+                    Hire Me
+                </div>
+
+                <div className="download-cv">
+                    Download CV
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
